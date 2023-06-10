711666393d73a81899e336aaa2b08e1fa3970d8b
muntasir <mjmullick0608@gmail.com>
2023-06-11 03:46:19 +0530

    renamed and added comments to mprintf.c

---
+++ mprintf.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <time.h>
+
+int mprintf(const char *str,...){   
+    va_list ptr;
+    va_start(ptr,str);//str is the last known variable name
+    char token[1000];
+    int k=0;//index of where to store the characters of str in token
+    for(int i=0;str[i]!='\0';i++){
+        token[k++]=str[i];
+        if(str[i+1]=='%'||str[i+1]=='\0'){
+            token[k]='\0';k=0;//the token becomes a string when terminated with '\0'
+            if(token[0]!='%'){
+                fprintf(stdout,"%s",token);//printing the whole token if it is not a format specifier
+            }
+            else{
+                int j=1;char ch1=0;
+                while((ch1=token[j++])<58) {}//this loop is required when printing formatted value like 0.2f, when ch1='f' loop ends
+                if(ch1=='i'||ch1=='d'|| ch1=='u'||ch1=='h'){
+                    fprintf(stdout,token,va_arg(ptr,int));
+                }
+                else if(ch1=='c'){
+                    fprintf(stdout,token,va_arg(ptr,int));//gcc compiler promotes char in va_arg to int by default
+                }
+                else if(ch1=='f'){
+                    fprintf(stdout,token,va_arg(ptr,double));//gcc compiler promotes float in va_arg to double by default
+                }
+                else if(ch1=='l'){
+                    char ch2=token[2];
+                    if(ch2=='u'||ch2=='d'||ch2=='i'){
+                        fprintf(stdout,token,va_arg(ptr,long));
+                    }
+                    else if(ch2=='f'){
+                        fprintf(stdout,token,va_arg(ptr,double));
+                    }
+                }
+                else if(ch1=='L'){
+                    char ch2=token[2];
+                    if(ch2=='u'||ch2=='d'||ch2=='i'){
+                        fprintf(stdout,token,va_arg(ptr,long long));
+                    }
+                    else if(ch2=='f'){
+                        fprintf(stdout,token,va_arg(ptr,long double));
+                    }
+                }
+                else if(ch1=='s'){
+                    fprintf(stdout,token,va_arg(ptr,char*));
+                }
+                else{
+                    fprintf(stdout,"%s",token);//print the whole token if no case is matched
+                }
+            }
+        }
+    }
+    va_end(ptr);
+    return 0;
+}
+int main() {
+    clock_t start=clock();
+    printf("The quick %i brown %.4f fox %c jumped over %li the lazy %s dog", 23, 20.23, 'x', 20202020,"The rise of Hmans");
+    printf("\n%fs\n",(float)(clock()-start)/CLOCKS_PER_SEC);
+    start=clock();
+    mprintf("The quick %i brown %.4f fox %c jumped over %li the lazy %s dog", 23, 20.23, 'x', 20202020,"The rise of Humans");
+    printf("\n%fs\n",(float)(clock()-start)/CLOCKS_PER_SEC);
+    return 0;
+}
---
+++ mscanf.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+
+int mscanf(const char *str,...){
+    char token[sizeof(str)];int k=0;
+    va_list ptr;
+    va_start(ptr,str);
+    for(int i=0;str[i]!='\0';i++){
+        token[k++]=str[i];        
+        if(str[i+1]=='%' || str[i+1]=='\0'){
+            token[k]='\0';k=0;
+            if(token[0]=='%'){
+                char ch1=token[1];
+                if(ch1=='i'||ch1=='d'|| ch1=='u'){
+                    fscanf(stdin,"%i",va_arg(ptr,int*));
+                    fprintf(stdout,"%s",token+2);
+                }
+                else if(ch1=='h'){
+                    fscanf(stdin,"%hi",va_arg(ptr,short*));
+                    fprintf(stdout,"%s",token+2);
+                }
+                else if(ch1=='c'){
+                    char c;
+                    while((c=fgetc(stdin))=='\n'||c==' '||c==EOF){}
+                    *va_arg(ptr,char*)=c;
+                    fprintf(stdout,"%s",token+2);
+                }
+                else if(ch1=='f'){
+                    fscanf(stdin,"%f",va_arg(ptr,float*));
+                    fprintf(stdout,"%s",token+2);
+                }
+                else if(ch1=='l'){
+                    char ch2=token[2];
+                    if(ch2=='u'||ch2=='d'||ch2=='i'){
+                        fscanf(stdin,"%li",va_arg(ptr,long*));
+                        fprintf(stdout,"%s",token+3);
+                    }
+                    else if(ch2=='f'){
+                        fscanf(stdin,"%lf",va_arg(ptr,double*));
+                        fprintf(stdout,"%s",token+3);
+                    }
+                }
+                else if(ch1=='L'){
+                    char ch2=token[2];
+                    if(ch2=='u'||ch2=='d'||ch2=='i'){
+                        fscanf(stdin,"%Li",va_arg(ptr,long long*));
+                        fprintf(stdout,"%s",token+3);
+                    }
+                    else if(ch2=='f'){
+                        fscanf(stdin,"%Lf",va_arg(ptr,long double*));
+                        fprintf(stdout,"%s",token+3);
+                    }
+                    }
+                else if(ch1=='s'){
+                    fscanf(stdin,"%s",va_arg(ptr,char*));
+                    fprintf(stdout,"%s",token+2);
+                }
+            }
+            else{
+                
+                fprintf(stdout,"%s",token);
+            }
+        }
+    }
+    return 0;
+}
+int main(){
+    int a;float b; char c;char s[20]; short d;long long e;
+    mscanf("Scan this %i HEllo %c %sBYEBYe",&a,&c,s);
+    printf("%i -%c- >%s<",a,c,s);
+    return 0;
+}
+//bugs: xscanf("%i %c %s",&a,&c); returns segfault whereas scanf doesnt
